4c6ef0a784353dedd2260112650567a70d524f56
MohamedAmine-Boussetta <2369634@cmontmorency.qc.ca>
2024-09-16 17:11:15 -0400

    Scene Jeu terminer

--- src/js/scenes/Jeu.js
+++ src/js/scenes/Jeu.js
@@ -4,6 +4,27 @@ class Jeu extends Phaser.Scene {
     }
 
     preload() {
+        this.load.image('BG', './assets/images/ui/Main_Menu/BG.png');
+        this.load.image('exitBtn', './assets/images/ui/Main_Menu/Exit_BTN.png');
+    }
+
+    create() {
+        //HUD
+        const hudContainer = this.add.container(0, 0).setDepth(1);
+
+
+        //BG
+        let bg = this.add.image(config.width / 2, config.height / 2, 'BG').setAngle(90);
+
+        //exitBtn
+        let exitBtn = this.add.image(config.width / 2, config.height / 2 + 100, 'exitBtn');
+        hudContainer.add(exitBtn);
+
+        exitBtn.setInteractive();
+
+        exitBtn.on('pointerdown', () => {
+            this.scene.start('accueil');
+        });
 
     }
 }
