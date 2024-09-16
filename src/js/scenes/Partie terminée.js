ba94afbfa90e2db5aac07c8a07868844c2f0d753
MohamedAmine-Boussetta <2369634@cmontmorency.qc.ca>
2024-09-16 17:40:03 -0400

    Section partie terminer fini

---
+++ src/js/scenes/Partie terminée.js
@@ -0,0 +1,40 @@
+class Guide extends Phaser.Scene {
+    constructor() {
+        super({ key: 'perdu' });
+    }
+
+    preload() {
+        this.load.image('BG', './assets/images/ui/Main_Menu/BG.png');
+        this.load.image('mainMenu', './assets/images/ui/Buttons/BTNs/Play_BTN.png');
+        this.load.image('replay', './assets/images/ui/Buttons/BTNs/Replay_BTN.png');
+    }
+
+    create() {
+        //HUD
+        const hudContainer = this.add.container(0, 0).setDepth(1);
+
+
+        //BG
+        let bg = this.add.image(config.width / 2, config.height / 2, 'BG').setAngle(90);
+
+        //main menu
+        let mainMenu = this.add.image(config.width / 2, config.height / 2 + 100, 'mainMenu').setScale(0.5).setOrigin(0, 1);
+        hudContainer.add(mainMenu);
+
+        //replay
+        let replay = this.add.image(config.width / 2 + 100, config.height / 2 + 100, 'replay').setScale(0.5).setOrigin(0, 1);
+        hudContainer.add(replay);
+
+        mainMenu.setInteractive();
+        replay.setInteractive();
+
+        mainMenu.on('pointerdown', () => {
+            this.scene.start('accueil');
+        });
+
+        replay.on('pointerdown', () => {
+            this.scene.start('jeu');
+        });
+
+    }
+}
